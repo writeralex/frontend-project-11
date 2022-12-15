913c2430237911f3d678928ac48d613484b87c6a
Alexandra.Yakuba <alexandra.yakuba@inbox.ru>
2022-12-15 14:00:59 +0300

    install webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const module = {
+  entry: './src/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  }
+}
+export default module;
