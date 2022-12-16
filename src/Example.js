81c3db7bc8235805fa9a21d31fedc08878cbbbd8
Alexandra.Yakuba <alexandra.yakuba@inbox.ru>
2022-12-16 19:32:06 +0300

    webpack

--- src/Example.js
+++ src/Example.js
@@ -1,5 +1,6 @@
 // @ts-check
 
+
 export default class Example {
     constructor(element) {
       this.element = element;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 
 import init from './init.js';
 import './style.css';
+import  './styles.scss';
+import  'bootstrap';
 
 init();
